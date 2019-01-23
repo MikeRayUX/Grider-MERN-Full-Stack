effcdf75f77bdb7bf6abfc7439d7138e6bb7cfe6
MichaelArriaga <m.arriaga.smb@gmail.com>
2019-01-23 11:35:51 -0800

    Finish Header login/logout

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,18 +1,50 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link } from "react-router-dom";
 
 class Header extends Component {
+  // change 'login with google' depending on user logged in
+  renderContent() {
+    switch (this.props.auth) {
+      case null:
+        // return nothing so that it doesn't jump on the screen while waiting for request
+        return;
+      case false:
+        return (
+          <li>
+            <a href="/auth/google">Login With Google</a>
+          </li>
+        );
+      default:
+        return (
+          <li>
+            <a href="/api/logout">Logout</a>
+          </li>
+        );
+    }
+  }
   render() {
     return (
-        <nav>
-          <div className="nav-wrapper">
-            <a className="left brand-logo">Emaily</a>
-            <ul id="nav-mobile" className="right">
-              <li><a>Login With Google</a></li>
-            </ul>
-          </div>
-        </nav>
-    )
+      <nav>
+        <div className="nav-wrapper">
+          <Link
+            // if logged in, have nav link render surveys component, otherwise render landing
+            to={this.props.auth ? "/surveys" : "/"}
+            className="left brand-logo"
+          >
+            Emaily
+          </Link>
+          <ul id="nav-mobile" className="right">
+            {this.renderContent()}
+          </ul>
+        </div>
+      </nav>
+    );
   }
 }
 
-export default Header
+function mapStateToProps(state) {
+  return { auth: state.auth };
+}
+
+export default connect(mapStateToProps)(Header);
--- client/src/reducers/authReducer.js
+++ client/src/reducers/authReducer.js
@@ -1,8 +1,12 @@
 // records whether or not the user is logged in
+import { FETCH_USER } from "../actions/types";
 
-export default function(state = {}, action) {
-  console.log(action);
+export default function(state = null, action) {
+  // console.log(action);
   switch (action.type) {
+    case FETCH_USER:
+      // empty string is a falsy value in javascript
+      return action.payload || false;
     default:
       return state;
   }
